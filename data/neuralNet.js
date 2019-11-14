438e5eb759cd3036613df8176efeebc92fbd734a
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-11-14 17:00:37 -0600

    neural net trains data; WIP accuracy + config options

--- data/neuralNet.js
+++ data/neuralNet.js
@@ -1,4 +1,4 @@
-const { neuralNetwork } = require('brain.js');
+const { NeuralNetwork } = require('brain.js');
 const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
@@ -6,7 +6,7 @@ const minMaxByField = require('./findMaxMin'); //arr of objects: {field {min, ma
 
 //read data file as synch function; returns raw data as array of strings
 const rawTrainingData = fs
-  .readFileSync(path.join(__dirname + '/normalizeData.csv'), 'utf8')
+  .readFileSync(path.join(__dirname + '/train.csv'), 'utf8')
   .split('\n');
 
 //extract headers
@@ -16,7 +16,7 @@ const headers = rawTrainingData[0]
 
 let sampleSize = rawTrainingData.length - 1;
 
-//NeuralNetwork takes [{input: {label: value}, {ouput: {label: value}}}], with normalized values btwn 0 and 1; map over data, setting labels for each datapoint
+//Format training data ([{input: {label: value}, {output: {label: value}}] with normalized values between 0 and 1
 const allTrainingData = rawTrainingData.slice(1, sampleSize).map(row => {
   return row.split(',').reduce((dataObj, dataVal, index) => {
     let header = headers[index];
@@ -25,7 +25,7 @@ const allTrainingData = rawTrainingData.slice(1, sampleSize).map(row => {
       //Diagnosis data normalized as 0 (benign[B]) or 1 (malignant[M])
       dataVal = dataVal === 'M' ? 1 : 0;
     } else if (header !== 'id') {
-      //Other data normalized as | (value - min) | / range
+      //Other data normalized as |(value - min)| / range
       if (minMaxByField.hasOwnProperty(header)) {
         let min = minMaxByField[header].min;
         let max = minMaxByField[header].max;
@@ -37,17 +37,30 @@ const allTrainingData = rawTrainingData.slice(1, sampleSize).map(row => {
         dataVal = normalizedToDecimal;
       }
     }
+    //set label for data point
     dataObj[header] = dataVal;
     return dataObj;
   }, {});
 });
 
-//input has every field except id + diagnosis; (Note: omit slower than pick)
-const input = allTrainingData.map(row => _.omit(row, ['id', 'diagnosis']));
+//input will have all fields except id + diagnosis; only output diagnosis
+//NOTE TO SELF: _.Omit slower than _.pick
+const cleanTrainingData = allTrainingData.map(row => ({
+  input: _.omit(row, ['id', 'diagnosis']),
+  output: _.pick(row, ['diagnosis']),
+}));
 
-//output only has diagnosis label;
-const output = allTrainingData.map(row => _.pick(row, ['diagnosis']));
+//NOTE TO SELF FOR LATER: Consider shuffling training data
 
-//Consider shuffling data
+//initialize neural network
+const config = {
+  activiation: 'sigmoid',
+  // hiddenLayers: [4],
+  // iterations: 156,
+  learningRate: 0.5,
+};
+const net = new NeuralNetwork(config);
+// net.train(cleanTrainingData);
 
+console.log('Done training!', net.train(cleanTrainingData));
 //simple feed forward neural network; can play around with options for hiddenLayers & activiation; consider accuracy function
