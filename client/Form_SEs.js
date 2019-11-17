96ba4e7885e28740be81cc30c61180d4c760f0dc
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-11-17 17:23:16 -0600

    added typography tag for formatting

--- client/Form_SEs.js
+++ client/Form_SEs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import renderField from './renderField';
-import Button from '@material-ui/core/Button';
+import { Button, Typography } from '@material-ui/core';
 
 const StandardErrors = props => {
   const { handleSubmit, prevPage } = props;
@@ -9,92 +9,96 @@ const StandardErrors = props => {
 
   return (
     <div>
-      <h3>Please provide the standard error (SE) for each field.</h3>
-      <form onSubmit={handleSubmit}>
-        <Field
-          name="radiusSE"
-          component={renderField}
-          type="number"
-          label="Radius SE: "
-        />
-        <br />
-        <Field
-          name="textureSE"
-          component={renderField}
-          type="number"
-          label="Texture SE: "
-        />
-        <br />
-        <Field
-          name="perimeterSE"
-          component={renderField}
-          type="number"
-          label="Perimeter SE: "
-        />
-        <br />
-        <Field
-          name="areaSE"
-          component={renderField}
-          type="number"
-          label="Area SE: "
-        />
-        <br />
-        <Field
-          name="smoothnessSE"
-          component={renderField}
-          type="number"
-          label="Smoothness SE"
-        />
-        <br />
-        <Field
-          name="compactnessSE"
-          component={renderField}
-          type="number"
-          label="Compactness SE"
-        />
-        <br />
-        <Field
-          name="concavitySE"
-          component={renderField}
-          type="number"
-          label="Concavity SE"
-        />
-        <br />
-        <Field
-          name="concavePointsSE"
-          component={renderField}
-          type="number"
-          label="Concave Points SE"
-        />
-        <br />
-        <Field
-          name="symmetrySE"
-          component={renderField}
-          type="number"
-          label="Symmetry SE"
-        />
-        <br />
-        <Field
-          name="fractalDimensionSE"
-          component={renderField}
-          type="number"
-          label="Fractal Dimension SE"
-        />
-        <br />
-        <div>
-          <Button
-            type="button"
-            color="secondary"
-            variant="outlined"
-            onClick={prevPage}
-          >
-            Previous Page
-          </Button>
-          <Button type="submit" color="secondary" variant="outlined">
-            Next Page
-          </Button>
-        </div>
-      </form>
+      <Typography>
+        <h3>Please provide the standard error (SE) for each field.</h3>
+      </Typography>
+      <Typography>
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="radiusSE"
+            component={renderField}
+            type="number"
+            label="Radius SE: "
+          />
+          <br />
+          <Field
+            name="textureSE"
+            component={renderField}
+            type="number"
+            label="Texture SE: "
+          />
+          <br />
+          <Field
+            name="perimeterSE"
+            component={renderField}
+            type="number"
+            label="Perimeter SE: "
+          />
+          <br />
+          <Field
+            name="areaSE"
+            component={renderField}
+            type="number"
+            label="Area SE: "
+          />
+          <br />
+          <Field
+            name="smoothnessSE"
+            component={renderField}
+            type="number"
+            label="Smoothness SE"
+          />
+          <br />
+          <Field
+            name="compactnessSE"
+            component={renderField}
+            type="number"
+            label="Compactness SE"
+          />
+          <br />
+          <Field
+            name="concavitySE"
+            component={renderField}
+            type="number"
+            label="Concavity SE"
+          />
+          <br />
+          <Field
+            name="concavePointsSE"
+            component={renderField}
+            type="number"
+            label="Concave Points SE"
+          />
+          <br />
+          <Field
+            name="symmetrySE"
+            component={renderField}
+            type="number"
+            label="Symmetry SE"
+          />
+          <br />
+          <Field
+            name="fractalDimensionSE"
+            component={renderField}
+            type="number"
+            label="Fractal Dimension SE"
+          />
+          <br />
+          <div>
+            <Button
+              type="button"
+              color="secondary"
+              variant="outlined"
+              onClick={prevPage}
+            >
+              Previous Page
+            </Button>
+            <Button type="submit" color="secondary" variant="outlined">
+              Next Page
+            </Button>
+          </div>
+        </form>
+      </Typography>
     </div>
   );
 };
--- client/Form_Worsts.js
+++ client/Form_Worsts.js
@@ -1,100 +1,104 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import renderField from './renderField';
-import Button from '@material-ui/core/Button';
+import { Button, Typography } from '@material-ui/core';
 
 const Worsts = props => {
   const { handleSubmit, prevPage } = props;
   return (
     <div>
-      <h3>
-        Please provide the "worst" (mean of 3 largest values) for each field.
-      </h3>
-      <form onSubmit={handleSubmit}>
-        <Field
-          name="radiusWorst"
-          component={renderField}
-          type="number"
-          label="Radius Worst: "
-        />
-        <br />
-        <Field
-          name="textureWorst"
-          component={renderField}
-          type="number"
-          label="Texture Worst: "
-        />
-        <br />
-        <Field
-          name="perimeterWorst"
-          component={renderField}
-          type="number"
-          label="Perimeter Worst: "
-        />
-        <br />
-        <Field
-          name="areaWorst"
-          component={renderField}
-          type="number"
-          label="Area Worst: "
-        />
-        <br />
-        <Field
-          name="smoothnessWorst"
-          component={renderField}
-          type="number"
-          label="Smoothness Worst: "
-        />
-        <br />
-        <Field
-          name="compactnessWorst"
-          component={renderField}
-          type="number"
-          label="Compactness Worst: "
-        />
-        <br />
-        <Field
-          name="concavityWorst"
-          component={renderField}
-          type="number"
-          label="Concavity WOrst: "
-        />
-        <br />
-        <Field
-          name="concavePointsWorst"
-          component={renderField}
-          type="number"
-          label="Concave Points Worst: "
-        />
-        <br />
-        <Field
-          name="symmetryWorst"
-          component={renderField}
-          type="number"
-          label="Symmetry Worst: "
-        />
-        <br />
-        <Field
-          name="fractalDimensionWorst"
-          component={renderField}
-          type="number"
-          label="Fractal Dimension Worst: "
-        />
-        <br />
-        <div>
-          <Button
-            type="button"
-            color="secondary"
-            variant="outlined"
-            onClick={prevPage}
-          >
-            Previous Page
-          </Button>
-          <Button type="submit" color="secondary" variant="outlined">
-            Submit Data
-          </Button>
-        </div>
-      </form>
+      <Typography>
+        <h3>
+          Please provide the "worst" (mean of 3 largest values) for each field.
+        </h3>
+      </Typography>
+      <Typography>
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="radiusWorst"
+            component={renderField}
+            type="number"
+            label="Radius Worst: "
+          />
+          <br />
+          <Field
+            name="textureWorst"
+            component={renderField}
+            type="number"
+            label="Texture Worst: "
+          />
+          <br />
+          <Field
+            name="perimeterWorst"
+            component={renderField}
+            type="number"
+            label="Perimeter Worst: "
+          />
+          <br />
+          <Field
+            name="areaWorst"
+            component={renderField}
+            type="number"
+            label="Area Worst: "
+          />
+          <br />
+          <Field
+            name="smoothnessWorst"
+            component={renderField}
+            type="number"
+            label="Smoothness Worst: "
+          />
+          <br />
+          <Field
+            name="compactnessWorst"
+            component={renderField}
+            type="number"
+            label="Compactness Worst: "
+          />
+          <br />
+          <Field
+            name="concavityWorst"
+            component={renderField}
+            type="number"
+            label="Concavity WOrst: "
+          />
+          <br />
+          <Field
+            name="concavePointsWorst"
+            component={renderField}
+            type="number"
+            label="Concave Points Worst: "
+          />
+          <br />
+          <Field
+            name="symmetryWorst"
+            component={renderField}
+            type="number"
+            label="Symmetry Worst: "
+          />
+          <br />
+          <Field
+            name="fractalDimensionWorst"
+            component={renderField}
+            type="number"
+            label="Fractal Dimension Worst: "
+          />
+          <br />
+          <div>
+            <Button
+              type="button"
+              color="secondary"
+              variant="outlined"
+              onClick={prevPage}
+            >
+              Previous Page
+            </Button>
+            <Button type="submit" color="secondary" variant="outlined">
+              Submit Data
+            </Button>
+          </div>
+        </form>
+      </Typography>
     </div>
   );
 };
