8840ec8153adc718094c1b43baa9e9b0cab3a621
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-11-17 15:53:24 -0600

    deleted extra text

--- client/RunTestData.js
+++ client/RunTestData.js
@@ -3,34 +3,13 @@ import { Typography } from '@material-ui/core';
 import TestDataTable from './TestDataTable';
 import StatsPopover from './StatsPopover';
 
-// const useStyles = makeStyles(theme => ({
-//   typography: {
-//     padding: theme.spacing(2),
-//   },
-// }));
-
 const RunTestData = () => {
-  // const [isClicked, setClicked] = useState(false);
-
-  // const [open, setOpen] = useState(false);
-
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
-
   return (
     <div>
       <br />
       <Typography>Last Test Run (n=284)</Typography>
       <br />
       <StatsPopover />
-      {/* <Button type="button" variant="outlined" color="secondary">
-        View Accuracy Stats
-      </Button> */}
       <br />
       <br />
       <TestDataTable />
